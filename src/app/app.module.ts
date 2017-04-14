aa4dbf75690ce2f9231de56023f0f43a5b07f50b
Oussemalaamiri <laamiri.oussema@gmail.com>
2017-04-14 10:34:52 +0100

    oussema integ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,69 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule,Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './posts/post.component';
+import { addBeacon } from './posts/addbeacon.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { ChartsModule } from 'ng2-charts';
+import 'chart.js/src/chart.js'
+import { PostsService } from './posts/posts.service';
 
-import { AppComponent }  from './app.component';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'posts/:id',
+    component: PostComponent,
+    templateUrl:'./posts/post.component.html'
+  },
+   {
+    path: 'beacons/add',
+    component: addBeacon,
+    templateUrl:'./posts/post.component.html'
+  }
+];
+
+
+
+
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    addBeacon,  
+    NavbarComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ChartsModule,
+    RouterModule.forRoot(ROUTES),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAL7c9QdPAM76YPaaEBU2TZGuhMVx4GDFI'
+    })
+  ],
+  providers: [PostsService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from './posts.service';
+import {ActivatedRoute,Router,Params} from '@angular/router'
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+
+
+export class PostsComponent implements OnInit {
+ posts: any = [];
+  title: string = 'My first angular2-google-maps project';
+  zoom:number=12;
+ streetViewControl :boolean=true;
+ backgroundColor:string="#E6E6E6";
+lat:Number=48.854624;
+lng:Number=2.324209;
+
+public pieChartLabels:string[] = ['Working Beacons', 'Out of Order Beacons'];
+  public pieChartData:number[] = [5, 1];
+  public pieChartType:string = 'pie';
+  constructor(private postsService: PostsService,private route : ActivatedRoute,private router:Router) { }
+
+  ngOnInit() {
+this.postsService.getAllPosts().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+getPost(id){
+this.postsService.getPostById(id);
+}
+
+
+
+
+
+
+
+
+
+
+deleteBeacon(id) {
+  this.postsService.deleteBeacon(id).then((result) => {
+    this.router.navigate(['/posts']);
+  }, (err) => {
+    console.log(err);
+  });
+}
+
+// oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+// firstDate = Date.now();
+// secondDate = new Date(2008,01,22);
+
+// diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+
+
+
+
+
+
+ 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
