373d6de6a34f8531189639b28a783735d4652095
Houssem Missaoui <houssem.missaoui@esprit.tn>
2017-04-14 15:39:11 +0100

    adjusting users.service.ts warning

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -3,12 +3,9 @@ import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
 @Injectable()
 export class UsersService {
-
   constructor(private http : Http) { }
   loadUsersPosition(){
     return this.http.get('http://localhost:3000/api/usersByPos')
       .map(res => res.json());
-
   }
-
 }
